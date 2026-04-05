2dea2fb8e04e3414a24f5150f3616891d0386486
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2026-04-05 18:16:22 +0530

    added deploying configuration

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 1. Tells Next.js to build static HTML files
+  output: "export", 
+  
+  // 2. VERY IMPORTANT: Replace this with your exact GitHub repository name
+  basePath: "/scroll-animation-assignment", 
+
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
+    // 3. Required for static exports on GitHub Pages, even for local images
+    unoptimized: true, 
   },
 };
 
